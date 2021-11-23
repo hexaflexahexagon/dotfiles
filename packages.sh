9be66a061c3af7d0d77514eea4775f15b367c105
hexaflexahexagon <hexaflexahexagon@users.noreply.github.com>
2021-11-23 18:13:31 -0500

    feat: add packages.sh file
    lists all packes that may be worth installing

---
+++ packages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# sudo apt update && sudo apt upgrade
+# sudo apt install -y $( ./packages )
+
+# programs
+echo "git"
+echo "curl"
+echo "ffmpeg"
+echo "grep"
+echo "vim"
+echo "shellcheck"
+echo "hexedit"
+echo "python3"
+echo "less"
+echo "man"
+echo "screen"
+
+# networking
+echo "netutils" 
+echo "dnsutils" # for dig, nslookup
+echo "traceroute"
+echo "net-tools" # netstat, route, arp

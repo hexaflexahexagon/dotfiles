edfd3cdedfd8d9eaf6edffc90625f96d9e8789e4
hexaflexahexagon <hexaflexahexagon@users.noreply.github.com>
2021-12-28 19:19:34 -0500

    feat: add `pngcheck` to packages.sh and add new category 'forensics'

--- packages.sh
+++ packages.sh
@@ -17,9 +17,12 @@ echo "python3"
 echo "less"
 echo "man"
 echo "screen"
-echo "file"
 #echo "gh"		# I use this sometimes but it's not in default package repos, see: https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+
+# forensics
+echo "file"
 echo "binutils" # strings
+echo "pngcheck"
 
 # networking
 echo "netutils" 
